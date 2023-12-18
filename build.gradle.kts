376a484ff2b333e05bb92f2ac51d1855b5d1d17a
Erik Koppenhagen <erik.koppenhagen@iits-consulting.de>
2023-12-18 21:31:25 +0100

    setup: add detekt framework for code analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,30 @@ kotlin {
 }
 
 plugins {
-    kotlin("jvm") version "1.9.21"
+    val kotlinVersion = "1.9.21"
+    val detektVersion = "1.23.4"
+
+    kotlin("jvm") version kotlinVersion
+    id("io.gitlab.arturbosch.detekt") version detektVersion
 }
 
 dependencies {
+    val detektVersion = "1.23.4"
     val junitJupiterVersion = "5.10.1"
     val kotestVersion = "5.8.0"
+
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-libraries:$detektVersion")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+}
+
+detekt {
+    toolVersion = "1.23.4"
+    config.setFrom(file("detekt.yml"))
+    buildUponDefaultConfig = true
 }
 
 tasks.test(Test::useJUnitPlatform)
